0b32d24149b02c53c3011cffe1d7d05ed242e809
frake23 <dimsacom@gmail.com>
2021-12-27 14:44:23 +0300

    test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -4,9 +4,23 @@ describe('Login', () => {
         cy.get('button').click();
 
         cy.url().should('include', '/login');
-        
+        cy.get('a[href="/register"]').click();
+        cy.url().should('include', '/register');
+
+        cy.fixture('user').then(user => {
+            cy.get('#email').type(user.email)
+            cy.get('#username').type(user.username)
+            cy.get('#password').type(user.password)
+            cy.get('button').click()
+            cy.url().should('include', '/')
+            cy.get('a[href="/bubbles/manage/all"]').click()
+            cy.url().should('include', '/bubbles/manage/all')
+        })
     }),
     it('should redirect on create bubble', () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('a[href="/bubbles/manage/all"]').click()
 
+        cy.url().should('include', '/login')
     })
 })
